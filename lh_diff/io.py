74866b020dd802a67d8e9e87b2fda4f456465d9c
BigDonalds <138994505+BigDonalds@users.noreply.github.com>
2025-11-14 11:03:24 -0500

    Refactor normalize_line and build_normalized_lines functions

--- lh_diff/io.py
+++ lh_diff/io.py
@@ -11,22 +11,17 @@ def read_file(filepath: str) -> List[str]:
     return lines
 
 
-def normalize_line(
-    line: str, remove_comments: bool = True, lowercase: bool = False
-) -> str:
+def normalize_line(line: str, remove_comments: bool = True, lowercase: bool = False) -> str:
     """
     remove whitespace and replace multiple spaces with one
     """
     line = line.strip()
-    line = re.sub(r"\s+", " ", line)
+    line = re.sub(r'\s+', ' ', line)
 
     if remove_comments:
-        line = re.sub(r"//.*", "", line)  # C/Java style
-        line = re.sub(r"#.*", "", line)  # Python/Unix style
-        line = re.sub(
-            r"\'\'\'.*?\'\'\'", "", line
-        )  # python block comments (though not integrated into the language natively, this is the standard form of block comments in python)
-        line = re.sub(r"/\*.*?\*/", "", line)  # Block comments
+        line = re.sub(r'//.*', '', line)        # C/Java style
+        line = re.sub(r'#.*', '', line)         # Python/Unix style
+        line = re.sub(r'/\*.*?\*/', '', line)   # Block comments
 
     # remove punctuation and braces
     # line = re.sub(r'[;,\(\)\{\}\[\]]', '', line)
@@ -37,16 +32,13 @@ def normalize_line(
     return line.strip()
 
 
-def build_normalized_lines(
-    filepath: str, remove_comments: bool = True, lowercase: bool = False
-) -> List[str]:
+def build_normalized_lines(filepath: str, remove_comments: bool = True, lowercase: bool = False) -> List[str]:
     """
     reads and normalizes all lines from a file\n
     returns list of normalized strings (empty lines kept as '')
     """
     raw_lines = read_file(filepath)
     # each line is normalized and contained in a list
-    norm_lines = [
-        normalize_line(line, remove_comments, lowercase) for line in raw_lines
-    ]
+    norm_lines = [normalize_line(line, remove_comments, lowercase) for line in raw_lines]
+    
     return norm_lines
